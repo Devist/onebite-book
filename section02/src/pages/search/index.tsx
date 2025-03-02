4ef056bb81e5b280b908126f7955ab7d252e6da6
Devist <aboutdh89@naver.com>
2025-03-02 12:34:18 +0900

    feat: 2-9 UI 완성하기

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,14 +1,14 @@
+import BookItem from "@/components/book-item";
 import SearchableLayout from "@/components/searchable-layout";
-import { useRouter } from "next/router";
-
+import books from "@/mock/books.json";
 export default function Page() {
-  const router = useRouter();
-
-  const { q } = router.query;
-
-  console.log(q);
-
-  return <h1>Search</h1>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: React.ReactNode) => {
