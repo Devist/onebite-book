76876f0ae38e3a9ac42b855c02337564a92ef2ef
Devist <aboutdh89@naver.com>
2025-03-06 01:12:31 +0900

    feat: 2-16 SSG 실습 - 동적 경로에 적용하기 - 폴백옵션-true

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -1,6 +1,7 @@
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import style from "./[id].module.css";
 import fetchOneBook from "@/lib/fetch-one-books";
+import { useRouter } from "next/router";
 
 export const getStaticPaths = () => {
   return {
@@ -13,10 +14,10 @@ export const getStaticPaths = () => {
      * 대체, 대비책, 보험. ex) book/4
      * 옵션 세 개 :
      * - false : 없는 페이지로 취급하도록 설정 404 Not Found 반환
-     * - blocking: 즉시 생성 (Like SSR). 이후 보관 (SSR 과 같은 문제 발생할 수도 있음)
-     * - true: 즉시 생성 + 페이지만 미리 반환 (Props 없는 페이지 빠르게 반환 = getStaticProps로부터 받은 데이터가 없는 페이지. props만 따로 반환)
+     * - blocking: [즉시 생성 (Like SSR)]. 이후 보관 (SSR 과 같은 문제 발생할 수도 있음)
+     * - true: [SSR 방식 + 데이터가 없는 폴백 상태의 페이지부터 반환] 즉시 생성 + 페이지만 미리 반환 (Props 없는 페이지 빠르게 반환 = getStaticProps로부터 받은 데이터가 없는 페이지. props만 따로 반환)
      */
-    fallback: "blocking",
+    fallback: true,
   };
 };
 
@@ -24,6 +25,13 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
   // 여기서 !로 단언할 수 있는 이유? 무조건 아이디가 하나 있어야만 접근할 수 있는 페이지
   const id = context.params!.id;
   const book = await fetchOneBook(Number(id));
+
+  // 페이지 컴포넌트의 문제가 발생했습니다가 아닌 404로 보냄
+  if (!book) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: { book },
@@ -33,7 +41,14 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
 export default function Page({
   book,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+
+  // fallback 상태 : 페이지 컴포넌트가 아직 서버로부터 데이터를 전달받지 못한 상태
+  if (router.isFallback) return "로딩중입니다";
+
+  // 로딩 끝났는데도 안 들어오면, 진짜 문제가 발생했습니다 보여줌
   if (!book) return "문제가 발생했습니다 다시 시도하세요";
+
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
 
   return (
