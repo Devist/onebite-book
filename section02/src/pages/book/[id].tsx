f9b2ac3c1c333569a00e398dfd86f8c81a18ab47
Devist <aboutdh89@naver.com>
2025-03-06 19:22:39 +0900

    feat: 2-19 fallback 상태에서도 기본적인 메타태그 보이도록 하기

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -36,7 +36,20 @@ export default function Page({
   const router = useRouter();
 
   // fallback 상태 : 페이지 컴포넌트가 아직 서버로부터 데이터를 전달받지 못한 상태
-  if (router.isFallback) return "로딩중입니다";
+  if (router.isFallback)
+    return (
+      <>
+        <title>한입북스</title>
+        {/* '/' 는 public 폴더 경로를 의미 */}
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스" />
+        <meta
+          property="og:description"
+          content="한입 북스에 등록된 도서들을 만나보세요"
+        />
+        <div>로딩중입니다</div>
+      </>
+    );
 
   // 로딩 끝났는데도 안 들어오면, 진짜 문제가 발생했습니다 보여줌
   if (!book) return "문제가 발생했습니다 다시 시도하세요";
@@ -45,6 +58,11 @@ export default function Page({
 
   return (
     <>
+      {/* 
+        다이나믹 SSG 이므로, fallback 옵션이 true 이므로,
+        최초 호출시에는 하단에 설정한 메타태그가 들어가 있지 않음
+        위의 return "로딩중입니다" 이기 때문에 나중에 추가되므로
+      */}
       <Head>
         <title>{title}</title>
         <meta property="og:image" content={coverImgUrl} />
