b4da5c85b2c347abe87b4a3f35357f7f0cbda009
Devist <aboutdh89@naver.com>
2025-03-07 22:00:07 +0900

    feat: 3-2 페이지 라우팅 설정(urlParams, or params)

---
+++ section03/src/app/search/page.tsx
@@ -0,0 +1,9 @@
+// async 붙일 수 있는 이유? 서버 컴포넌트이기 때문에
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q: string }>;
+}) {
+  const { q } = await searchParams;
+  return <div>Search 페이지 : {q}</div>;
+}
