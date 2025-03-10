9047aa663a34f6a2f6565dfd178a2e35f9d9b5c2
Devist <aboutdh89@naver.com>
2025-03-10 16:17:30 +0900

    feat: 5-2 풀라우트캐시 - 오직 클라이언트 컴포넌트로 동작하도록 설정

--- section05/src/components/searchbar.tsx
+++ section05/src/components/searchbar.tsx
@@ -6,7 +6,14 @@ import style from "./serachbar.module.css";
 
 export default function Searchbar() {
   const router = useRouter();
+  /**
+   * 인덱스 페이지를 빌드타임에 정적으로 생성하다가, 브라우저 요청으로부터 받는 부분이므로 해당 부분에서 에러
+   * 오직 클라이언트 컴포넌트로만 실행되게 하려면 "use client" 설정 외에도 아래와 같이 추가 조치 필요
+   * 해당 컴포넌트를 <Suspense></Suspense> 컴포넌트로 감싸줘야 함 (미완성 상태일 때 fallback으로 대체해줌)
+   *
+   */
   const searchParams = useSearchParams();
+
   const [search, setSearch] = useState("");
 
   const q = searchParams.get("q");
