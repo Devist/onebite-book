8f6b084c802bfde9400a7b0d462f0704f4635814
Devist <aboutdh89@naver.com>
2025-03-10 14:32:44 +0900

    feat: 4-2 데이터 캐시

--- section04/next.config.ts
+++ section04/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  /**
+   * 발생하는 모든 데이터페칭이 로그로서 출력이 됨
+   */
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 export default nextConfig;
