fa4f18661fc51407097bd52c0ef6300fa80fcb90
Devist <aboutdh89@naver.com>
2025-03-10 21:56:46 +0900

    feat: 5-4 라우트 세그먼트 옵션

--- section05/src/app/(with-searchbar)/search/page.tsx
+++ section05/src/app/(with-searchbar)/search/page.tsx
@@ -1,6 +1,13 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
 
+// export const dynamic = "force-static";
+// 검색 기능이 제대로 동작하지 않을 수 있음
+// 동적 함수의 값이 무조건 빈 값으로 설정되어 버리기 때문
+// 검색어가 무엇인지 알 수 없음
+
+// export const dynamic = "error";
+
 export default async function Page({
   searchParams,
 }: {
