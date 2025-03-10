387d6abefa886558f7a196822b54f3c7d3beb93a
Devist <aboutdh89@naver.com>
2025-03-10 16:57:23 +0900

    feat: 5-3 풀라우트캐시 - 동적 경로에 적용하기

--- section05/src/app/book/[id]/page.tsx
+++ section05/src/app/book/[id]/page.tsx
@@ -1,5 +1,24 @@
+import { notFound } from "next/navigation";
 import style from "./page.module.css";
 
+// id 1,2,3 아닌 경우에는 404로 가게됨. 동적으로 더이상 받을 파라미터가 존재하지 않아야된다 라고 설정하는것
+// export const dynamicParams = false;
+
+/**
+ * 정적인 파라미터를 생성하는 약속된 함수
+ * 주의할 점 1 : 문자열 데이터로만 명시해야 함
+ * 주의할 점 2 : 이 함수를 내보내게 되면, 페이지 컴포넌트 내부에 데이터 캐싱이 설정되지 않은 데이터 패칭이 존재해도
+ * 강제로 스테틱 페이지로 설정됨
+ *
+ * = getStaticPaths 의 앱라우터 버전이라 보면 됨
+ */
+export function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+}
+
+/**
+ * book/4는 실시간으로 다이나믹 페이지로 만들어짐 풀 라우트 캐시로 만들어짐
+ */
 export default async function Page({
   params,
 }: {
@@ -12,6 +31,9 @@ export default async function Page({
   );
 
   if (!response.ok) {
+    if (response.status === 404) {
+      notFound();
+    }
     return <div>오류가 발생했습니다...</div>;
   }
 
